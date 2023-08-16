29c2381d05b82044ff2c6290b7645f66811107fc
Edson Mendes <124837899+EdsonKL@users.noreply.github.com>
2023-08-16 10:54:30 -0300

    Atualização lanchonete

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -1,130 +1,116 @@
 
-
-let valorTotal = 0
-
-// Declaração de cardapio para posteriores calculos
-
-const cardapio = {
-    cafe: 3.00,
-    chantily: 1.50,
-    suco: 6.20,
-    sanduiche: 6.50,
-    queijo: 2.00,
-    salgado: 7.25,
-    combo1: 9.50,
-    combo2: 7.50,
-}
-
-// Classe CaixaDaLanchonete
-
 class CaixaDaLanchonete {
-
-    // Metodo para calcular o valor total da compra
-
+    constructor(){
+        this.carrinho = {}
+        this.formaPagamento = ["dinheiro", "credito", "debito"]
+        this.menu = {
+            "cafe" : 3.00,
+            "chantily" : 1.50,
+            "suco" : 6.20,
+            "sanduiche" : 6.50,
+            "queijo" : 2.00,
+            "salgado" : 7.25,
+            "combo1" : 9.50,
+            "combo2" : 7.50
+        }
+    }  
     calcularValorDaCompra(metodoDePagamento, itens) {
+        try {
+            this.verificarCarrinho(metodoDePagamento, itens)
+            this.adicionarNoCarrinho(itens)
+            this.validarExtras()
 
-        // possibilidades de compras no debito
+            const totalCarrinho = this.valorCarrinho()
+            const total = this.addPagamento(metodoDePagamento, totalCarrinho)
 
-    if (metodoDePagamento == 'debito') {
-        if (itens == '') {
-            return 'Não há itens no carrinho de compra!'
+            return `R$ ${total.toFixed(2).replace('.', ',')}`
+        } catch (error) {
+            return error
+        }      
+    }
+
+    addPagamento(metodoDePagamento, total){
+        if (metodoDePagamento == 'dinheiro')
+            return total * 0.95
+
+        if (metodoDePagamento == 'credito')
+            return total * 1.03
+
+        return total
+    } 
+
+    verificarItem(nomeItem){
+        if(this.menu.hasOwnProperty(nomeItem)){
+            return true
         }
-        if (itens == ['cafe,1'].toString()) {
-            valorTotal = cardapio.cafe.toFixed(2).replace(".",",")
-            return `R$ ${valorTotal}`
+            throw  'Item inválido!'
+    }
+
+    validarPrincipal(nome){
+        if(!this.carrinho.hasOwnProperty(nome)){
+            throw 'Item extra não pode ser pedido sem o principal'
         }
-        if (itens == ['cafe,1', 'sanduiche,1', 'queijo,1'].toString()) {
-            valorTotal = cardapio.cafe + cardapio.sanduiche + cardapio.queijo
-            valorTotal = valorTotal.toFixed(2).replace(".",",")
-            return `R$ ${valorTotal}`
-        }
-        if (itens == ['cafe,4', 'sanduiche,3', 'queijo,2'].toString()) {
-            valorTotal = ((cardapio.cafe * 4) + (cardapio.sanduiche * 3) + (cardapio.queijo * 2))
-            valorTotal = valorTotal.toFixed(2).replace(".",",")
-            return `R$ ${valorTotal}`
-        }
-        if (itens == ['pizza, 1'].toString()) {
-            return 'Item inválido!'
-        }
-        if (itens == ['cafe,1', 'queijo,1'].toString()) {
-            return 'Item extra não pode ser pedido sem o principal'
+            
+    }
+
+    validarExtras() {
+        for (const item of Object.keys(this.carrinho)) {
+            if (item == "chantily") {
+                this.validarPrincipal('cafe')
+            } 
+            if (item == "queijo") {
+                this.validarPrincipal('sanduiche')
+            }
         }
     }
 
-
-        // possibilidades de compras no dinheiro
-
-
-    if (metodoDePagamento == 'dinheiro') {
-        if (itens == '') {
-            return 'Não há itens no carrinho de compra!'
+    verificaQuant(nome, quant) {
+        if (quant == 0){
+            throw 'Quantidade inválida!'
         }
-        if (itens == ['cafe,1'].toString()) {
-            valorTotal = cardapio.cafe * 0.95
-            valorTotal = valorTotal.toFixed(2).replace('.',',')
-            return `R$ ${valorTotal}`
+            
+        if (nome == null){
+            throw 'Item inválido!'
         }
-        if (itens == ['cafe,1', 'sanduiche,1', 'queijo,1'].toString()) {
-            valorTotal = (cardapio.cafe + cardapio.sanduiche + cardapio.queijo) * 0.95
-            valorTotal = valorTotal.toFixed(2).replace(".",",")
-            return `R$ ${valorTotal}`
-        }
-        if (itens == ['cafe,4', 'sanduiche,3', 'queijo,2'].toString()) {
-            valorTotal = ((cardapio.cafe * 4) + (cardapio.sanduiche * 3) + (cardapio.queijo * 2)) * 0.95
-            valorTotal = valorTotal.toFixed(2).replace(".",",")
-            return `R$ ${valorTotal}`
-        }
-        if (itens == ['cafe,0'].toString()) {
-            return 'Quantidade inválida!'
-        }
-        if (itens == ['chantily,1'].toString()) {
-            return 'Item extra não pode ser pedido sem o principal'
-        }
-        
+            
     }
 
-        // possibilidades de compras no credito
 
-
-    if (metodoDePagamento == 'credito') {
-        if (itens == '') {
-            return 'Não há itens no carrinho de compra!'
+    verificarCarrinho(metodoDePagamento, itens){
+        if (!this.formaPagamento.includes(metodoDePagamento)){
+            throw 'Forma de pagamento inválida!'
         }
-        if (itens == ['cafe,1'].toString()) {
-            valorTotal = cardapio.cafe * 1.03
-            valorTotal = valorTotal.toFixed(2).replace('.',',')
-            return `R$ ${valorTotal}`
+            
+        if (itens == 0){
+            throw 'Não há itens no carrinho de compra!'
         }
-        if (itens == ['cafe,1', 'sanduiche,1', 'queijo,1'].toString()) {
-            valorTotal = (cardapio.cafe + cardapio.sanduiche + cardapio.queijo) * 1.03
-            valorTotal = valorTotal.toFixed(2).replace(".",",")
-            return `R$ ${valorTotal}`
-        }
-        if (itens == ['cafe,4', 'sanduiche,3', 'queijo,2'].toString()) {
-            valorTotal = ((cardapio.cafe * 4) + (cardapio.sanduiche * 3) + (cardapio.queijo * 2)) * 1.03
-            valorTotal = valorTotal.toFixed(2).replace(".",",")
-            return `R$ ${valorTotal}`
-        }
-        if (itens == ['1'].toString()) {
-            return 'Item inválido!'
-        }
-        if (itens == ['queijo,1'].toString()) {
-            return 'Item extra não pode ser pedido sem o principal'
-        }
-        if (itens == ['chantily,1', 'sanduiche,1'].toString()) {
-            return 'Item extra não pode ser pedido sem o principal'
-        }
-        
-    }
-
-        // Caso o metodo de pagamento seja invalido
-
-    if (metodoDePagamento != 'debito' || metodoDePagamento != 'credito' || metodoDePagamento != 'dinheiro' || metodoDePagamento == 'especie') {
-        return 'Forma de pagamento inválida!'
-    }
-
-    return ''
+            
     }
     
+    adicionarNoCarrinho(itens){
+        for (const item of itens) {
+            const [nome, quant] = item.split(',')
+
+            this.verificaQuant(nome, quant)
+            
+            if(this.verificarItem(nome)) {
+                this.carrinho[nome] = quant
+            }
+        }
+    }
+
+    valorCarrinho(){
+        let total = 0
+        for (const item of Object.keys(this.carrinho)){
+            total += this.menu[item] * this.carrinho[item]
+        }
+        return total
+    }
+
+    
 }
+const compra = new CaixaDaLanchonete().calcularValorDaCompra('dinheiro', ['cafe,1','chantily,1'])
+console.log(compra)
+
+
 export { CaixaDaLanchonete };
